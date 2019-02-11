6a44bb52f3c4d151e13bd15f4a0730c3a4d9dcc4
Adam Birem <adam.birem@protonmail.com>
2019-02-11 17:14:53 +0100

    Ajout du composant nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { EventAppComponent } from './event-app.component';
-import {EventListComponent} from './event-list.component';
-import {EventThumbnailComponent} from './event-thumbnail.component';
+import { EventAppComponent } from './event/event-app.component';
+import {EventListComponent} from './event/event-list.component';
+import {EventThumbnailComponent} from './event/event-thumbnail.component';
+import {NavbarComponent} from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventAppComponent,
     EventListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
--- src/app/event/event-app.component.ts
+++ src/app/event/event-app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <nav-bar></nav-bar>
     <event-list></event-list>
+
   `
 })
 export class EventAppComponent {
