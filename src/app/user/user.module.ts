42648698a3bcf8b40f75639dc85d254acfcfc40e
Adam Birem <adam.birem@protonmail.com>
2019-03-04 17:46:14 +0100

    Ajout d'un formulaire de login

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,19 @@ import {ProfileComponent} from './profile.component';
 import {RouterModule} from '@angular/router';
 import {userRoutes} from './user.route';
 import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     RouterModule.forChild(userRoutes),
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+
+    LoginComponent
   ],
   providers: [
 
